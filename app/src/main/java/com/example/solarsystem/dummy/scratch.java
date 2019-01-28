5a749dea5eab64c55c9569ba6428599b3eb75d42
Unknown <c.ettorre@gmail.com>
2019-01-28 21:05:29 +0100

    update

---
+++ app/src/main/java/com/example/solarsystem/dummy/scratch.java
@@ -0,0 +1,11 @@
+package com.example.solarsystem.dummy;
+
+class SampleUse {
+
+    public void useExtension(){
+        Planet p = new Planet();
+        PlanetUtils.getComposition(p);
+
+    }
+
+}
